cc449f8994eb0ac20cdff896f949cda9ff92ccac
Organic-Species <jarrel.thomas@gmail.com>
2023-07-22 09:17:27 -0400

    ex 6

--- ex5.py
+++ ex5.py
@@ -6,10 +6,13 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+convert_centi = height * 2.54
+convert_kilo = weight * 0.453592
+
 
 print(f"Let's talk about {name}.")
-print(f"He's {height} inches tall.")
-print(f"He's {weight} pounds heavy.")
+print(f"He's {height} inches tall or {int(convert_centi)} centimeters tall.")
+print(f"He's {weight} pounds heavy or {int(convert_kilo)} kilograms heavy.")
 print("Actually that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
