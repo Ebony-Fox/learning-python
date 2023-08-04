a648732dd9fd03147873f2996e1f6df5bd99187d
Jarrel-Thomas <jarrel.thomas@gmail.com>
2023-08-04 14:57:10 -0400

    New Notes Branch

--- ex9.py
+++ ex9.py
@@ -1,11 +1,16 @@
 # Here's some strange stuff, remember type it exactly.
 
+# creates variable days with a string of days of the week
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# Create a variable months with strings of months up until August using the new line chracter break (\n) to start each month on a new line
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# prints string Here are the days with days variable
 print("Here are the days: ", days)
+# prints string Here are the months with months variable
 print("Here are the months: ", months)
 
+# Using triple quotes to print multi-line string
 print("""
       There's something going on here.
       With the three double-quotes.
