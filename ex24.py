1e5c5ac8f07f860deb003c9ce8c805512459c58a
Jarrel-Thomas <jarrel.thomas@gmail.com>
2023-08-07 23:30:10 -0400

    Ex 23 - 24

---
+++ ex24.py
@@ -0,0 +1,12 @@
+print("Let's practice everything.")
+print('You\'d need to know \'bout escape with \\ that do:')
+print('\n newlines and \t tabs.')
+
+poem = """
+\tThe Lovely world
+with logic so firmly planted
+cannot discern \n the needs if love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere ther is none.
+"""
