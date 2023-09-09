eba9de2425ea794ebcf59a56b27552fb244dabca
Jarrel Thomas <jarrel.thomas@gmail.com>
2023-09-09 10:40:46 -0400

    Updated ex16.py

--- ex16.py
+++ ex16.py
@@ -22,12 +22,7 @@ line3 = input("line 3:")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(str(line1) + "\n" + str(line2) + "\n" + str(line3) + "\n")
 
 print("And finally, we close it.")
 target.close()
