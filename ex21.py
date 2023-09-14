4ce47f9562a167c02d457b93d06aafa557e68863
Jarrel Thomas <jarrel.thomas@gmail.com>
2023-09-13 22:40:20 -0400

    test

--- ex21.py
+++ ex21.py
@@ -29,3 +29,8 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 
 
 # A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
