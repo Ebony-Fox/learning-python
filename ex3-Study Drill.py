1a74020474d97bcbcfc97322cf5df5d31c3e0338
Jarrel Thomas <jarrel.thomas@gmail.com>
2024-07-22 13:08:15 -0400

    Updated ex3

--- ex3-Study Drill.py
+++ ex3-Study Drill.py
@@ -4,7 +4,22 @@ hourly_rate = 21.00
 state_tax = 0.06
 federal_tax = 0.05
 sick_days = 5
+
 work_week_hours = 40
 weeks_work = 52
+daily_hours = 8
 
-print(hourly_rate * work_week_hours * weeks_work)
+sick_time = sick_days * daily_hours
+total_work_hours = work_week_hours * weeks_work
+total_tax = state_tax + federal_tax
+
+adjusted_work_hours = total_work_hours - sick_time
+
+# Calculate the gross yearly salary
+gross_salary = hourly_rate * adjusted_work_hours
+
+# Calculate the net yearly salary after taxes
+net_salary = gross_salary * (1-total_tax)
+
+print("Gross Yearly Salary: $", gross_salary)
+print("Net Yearly Salary after Taxes (and sick days): $", net_salary)
