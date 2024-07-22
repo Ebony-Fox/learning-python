fc8b320981f773534ba5a0d35bdbf4f6931afa37
jarrel <jarrel.thomas@gmail.com>
2024-07-22 09:13:56 -0400

    ex3

---
+++ ex3-Study Drill.py
@@ -0,0 +1,10 @@
+# The following script will calculate your anual salary based on the your hourly pay.
+
+hourly_rate = 21.00
+state_tax = 0.06
+federal_tax = 0.05
+sick_days = 5
+work_week_hours = 40
+weeks_work = 52
+
+print(hourly_rate * work_week_hours * weeks_work)
--- ex3.py
+++ ex3.py
@@ -1,23 +1,34 @@
+# Prints the following string
 print("I will now count my chickens:")
 
+# Prints string and performs math equation
 print("Hens", 25+30/6)
+# Prints string and perfoms math equation
 print("Roosters", 100-25*3 % 4)
 
+# Prints string
 print("Now I will count the eggs:")
 
+# Prints math equation
 print(3+2+1-5+4 % 2-1/4+6)
 
+# Prints math string
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# Prints False boolean based on math equation
 print(3+2 < 5-7)
 
+# Prints string and math equation
 print("What is 3 + 2?", 3+2)
 print("What is 5 - 7?", 5-7)
 
+# Prints string
 print("Oh,that's why it's False")
 
+# Prints string
 print("How about some more.")
 
+# Following three lines print string and boolean variable based on math equation
 print("Is it greater?", 5 > -2)
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
