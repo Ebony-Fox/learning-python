54fde64f6ccd3c7cce346ae6da8d3599f255cdc6
Organic-Species <jarrel.thomas@gmail.com>
2024-07-22 15:43:08 -0400

    ex5 study drills

--- ex5.py
+++ ex5.py
@@ -1,18 +1,20 @@
-my_name = 'Zed A. Shaw'
-my_age = 35  # not a lie
-my_height = 74  # inches
-my_weight = 180  # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35  # not a lie
+height = 74  # inches
+height_converted = height * 2.54  # centimeters
+weight = 180  # lbs
+weight_converted = weight * 0.453592  # kilograms
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eys and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eys and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} T get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
